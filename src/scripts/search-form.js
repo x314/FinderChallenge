9b5e93f6294001ff688d83c1b7c0e5c6755c5e24
Alex Villanueva Mercado <alvimer2708@gmail.com>
2017-07-17 08:42:54 -0500

    disabled button search, enter search, autocomplete

--- src/scripts/search-form.js
+++ src/scripts/search-form.js
@@ -1,6 +1,7 @@
 
 function loadItems(data) {
     var b = '';
+    var arrayTitle = [];
     // console.log(actual_JSON.data);
     var first = _.first(data, 9);
     _.each(first, function(item) {
@@ -8,9 +9,21 @@ function loadItems(data) {
             '<h3>' + item.title + '</h3>' +
             '<p>' + item.teaser + '</p>' +
             '</div>';
+        arrayTitle.push(item.title);
     });
 
     $('.search-result').html(b);
+    // $('#txtSearch').attr('data-list', arrayTitle);
+    new Awesomplete('input#txtSearch', {
+      minChars: 3,
+      maxItems: 7,
+	    list: arrayTitle
+    });
+}
+
+function getChars() {
+  var chars = $('#txtSearch').val().length;
+  return chars;
 }
 
 var characterReg = /[`~!@#$%^&*()_°¬|+\-=?;:'",.<>\{\}\[\]\\\/]/gi;
@@ -21,15 +34,41 @@ $('#txtSearch').keyup(function(event) {
     }
 });
 
+$('#txtSearch').keyup(function(){
+  if(getChars() > 2){
+    $('#btnSearch').removeClass('pure-button-disabled');
+  }
+  else {
+    $('#btnSearch').addClass('pure-button-disabled');
+  }
+});
+
 $('#btnSearch').on('click', function() {
-    if ($('#txtSearch').val() != '') {
+    if(getChars() > 2){
         var x = _.filter(actual_JSON.data, function(item){
             if (item.title.toLowerCase().indexOf($('#txtSearch').val().toLowerCase()) >= 0)
                 return item
         });
         loadItems(x);
     }
-    else {
-        loadItems(actual_JSON.data);
+    // else {
+    //     loadItems(actual_JSON.data);
+    // }
+    return false;
+});
+
+$('.pure-form').on('keypress', function(e) {
+  if (e.which == 13) {
+    if(getChars() > 2){
+        var x = _.filter(actual_JSON.data, function(item){
+            if (item.title.toLowerCase().indexOf($('#txtSearch').val().toLowerCase()) >= 0)
+                return item
+        });
+        loadItems(x);
     }
+    // else {
+    //     loadItems(actual_JSON.data);
+    // }
+    return false;
+  }
 });
